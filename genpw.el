6e779339cf5bb6f7524d2a60d4795e176e3c7b94
Ivan Kelly <ivan@bleurgh.com>
2012-06-06 22:42:39 +0200

    Make reading of the password hidden

--- genpw.el
+++ genpw.el
@@ -7,8 +7,9 @@
 						 (if (and (> 126 c) (< 33 c)) c nil)))
 		   (string-to-list (sha1-binary seed))))) 0 10))
 
-(defun genpw-to-clipboard (seed)
-  (interactive "sSeed: ")
-  (let (( x-select-enable-clipboard t))
+(defun genpw-to-clipboard ()
+  (interactive)
+  (let ((seed (read-passwd "Seed: "))
+	( x-select-enable-clipboard t))
     (x-select-text (genpw seed))))
 
