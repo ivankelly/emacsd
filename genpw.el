db36c9699b68727bc4c8b9583548afc75d3b3983
test <test@sweetgreat-lm.barcelona.corp.yahoo.com>
2012-07-03 10:38:49 +0200

    Fix for genpw in emacs24 mailinit.el added for emacs writing

--- genpw.el
+++ genpw.el
@@ -5,7 +5,7 @@
   (substring (concat (delq nil 
 			   (mapcar (lambda (x) (let ((c (logand x 127)))
 						 (if (and (> 126 c) (< 33 c)) c nil)))
-		   (string-to-list (sha1-binary seed))))) 0 10))
+		   (string-to-list (sha1 seed nil nil t))))) 0 10))
 
 (defun genpw-to-clipboard ()
   (interactive)
